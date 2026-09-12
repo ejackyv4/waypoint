1c219bc411ed9561778278962b8c36d10eb17e64
Eric Jacky <ejacky@vant4ge.com>
2026-09-12 18:44:24 -0400

    Match iPad Safari user agent for course WebView

--- spike/mobile/App.js
+++ spike/mobile/App.js
@@ -4461,6 +4461,7 @@ function Player({ auth, program, onExit }) {
             ref={webRef}
             source={{ uri: url }}
             style={s.web}
+            userAgent="Mozilla/5.0 (iPad; CPU OS 17_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
             startInLoadingState
             renderLoading={() => (
               <View style={[s.center, s.loadingOverlay]}><ActivityIndicator color={C.brand} /></View>
